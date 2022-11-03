e1631b4238820967bebc804bdf74386bec952984
apollo79 <bummtschack@mail.de>
2022-11-03 14:19:03 +0100

    add uncommitted util file
    without this, ee922058dc76d7143a1fb8083eb0dbba253aafbc doesn't work

---
+++ util.test.ts
@@ -0,0 +1,20 @@
+import { assertStrictEquals } from "https://deno.land/std@0.161.0/testing/asserts.ts";
+import { describe, it } from "https://deno.land/std@0.161.0/testing/bdd.ts";
+import { strToMs } from "./util.ts";
+
+const values = {
+  "45ms": 45,
+  "3 second": 3000,
+  "4  hours": 14_400_000,
+  "5d": 432_000_000,
+  "1days 50sec": 86_450_000,
+  "45day and 373hours, 3 minutes": 5_230_980_000,
+};
+
+describe("strToMs", () => {
+  for (const key in values) {
+    it(`${key}`, () => {
+      assertStrictEquals(strToMs(key), values[key as keyof typeof values]);
+    });
+  }
+});
---
+++ util.ts
@@ -0,0 +1,62 @@
+export type Epoch =
+  | "millisecond"
+  | "milliseconds"
+  | "ms"
+  | "second"
+  | "seconds"
+  | "sec"
+  | "s"
+  | "minute"
+  | "minutes"
+  | "min"
+  | "m"
+  | "hour"
+  | "hours"
+  | "h"
+  | "day"
+  | "days"
+  | "d";
+
+export const epochs: Record<Epoch, number> = {
+  millisecond: 1,
+  milliseconds: 1,
+  ms: 1,
+  second: 1000,
+  seconds: 1000,
+  sec: 1000,
+  s: 1000,
+  minute: 60000,
+  minutes: 60000,
+  min: 60000,
+  m: 60000,
+  hour: 3600000,
+  hours: 3600000,
+  h: 3600000,
+  day: 86400000,
+  days: 86400000,
+  d: 86400000,
+};
+
+export const regexp =
+  /([\d.]+)\s*?(milliseconds?|ms|seconds?|sec|s|minutes?|min|m|hours?|h|days?|d)/gi;
+
+/**
+ * Converts a string containing a time expressed as human readable in a number of milliseconds
+ * @param str string to convert to milliseconds
+ * @returns number of milliseconds
+ */
+export function strToMs(str: string): number {
+  let ms = 0;
+
+  let match = null;
+
+  while ((match = regexp.exec(str))) {
+    const quantity = parseInt(match[1])!;
+
+    const epoch = match[2]! as Epoch;
+
+    ms += quantity * epochs[epoch];
+  }
+
+  return ms;
+}
