70b07d90a485ac916cd8a9cc99d3e5aacde832ef
duskflower <bummtschack@mail.de>
2025-05-14 13:27:06 +0200

    feat: simplify Timeout as it can extend the Interval class

--- src/Timeout.ts
+++ src/Timeout.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-explicit-any
-import { Timer, type TimerOptions } from "./Timer.ts";
-import { type Listener, TIMEOUT_MAX } from "../mod.ts";
+import type { TimerOptions } from "./Timer.ts";
+import { Interval, type Listener } from "../mod.ts";
 
 export interface TimeoutOptions<T extends any[] = any[]>
   extends TimerOptions<T> {}
@@ -24,15 +24,7 @@ export interface TimeoutOptions<T extends any[] = any[]>
  * yourService.addEventListener("error", () => abort.abort(), { once: true })
  * ```
  */
-export class Timeout<T extends any[] = any[]> extends Timer<T> {
-  declare readonly options: TimeoutOptions<T>;
-
-  protected override _ran = false;
-
-  get ran(): boolean {
-    return this._ran;
-  }
-
+export class Timeout<T extends any[] = any[]> extends Interval<T> {
   /**
    * @param cb The callback to get executed
    * @param delay The time to delay
@@ -46,64 +38,6 @@ export class Timeout<T extends any[] = any[]> extends Timer<T> {
     delay: number | string = 0,
     options: TimeoutOptions<T> = {},
   ) {
-    super(cb, delay, options);
-  }
-
-  /**
-   * Private run method to deal with the {@link Timer._running} property, as the method may call itself
-   */
-  #run() {
-    // if the remaining time to wait is in the range that is possible to handle for the native methods
-    if (this._timeLeft <= TIMEOUT_MAX) {
-      this._timer = globalThis.setTimeout(() => {
-        // call the callback with the given args
-        try {
-          this.cb(...this.options.args!);
-        } catch (e) {
-          if (!this.options.silent) {
-            this.abort();
-            throw e;
-          }
-        }
-
-        this.options.signal?.removeEventListener("abort", this.abort);
-
-        this._running = false;
-        this._ran = true;
-      }, this._timeLeft);
-    } else {
-      this._timer = globalThis.setTimeout(() => {
-        // update the time left
-        this._timeLeft -= TIMEOUT_MAX;
-
-        // run again with remaining time
-        this.#run();
-      }, TIMEOUT_MAX);
-    }
-
-    if (!this._persistent) {
-      this.unref();
-    }
-  }
-  /**
-   * Runs the interval.
-   * @returns the timer's id
-   */
-  run(): number {
-    if (this._running) {
-      console.warn(
-        "The timeout is already running. The call to run will be ignored",
-      );
-    } else if (this._isAborted) {
-      console.warn(
-        "The timeout has been aborted. The call to run will be ignored",
-      );
-    } else {
-      this.#run();
-
-      this._running = true;
-    }
-
-    return this.id;
+    super(cb, delay, { ...options, times: 1 });
   }
 }
